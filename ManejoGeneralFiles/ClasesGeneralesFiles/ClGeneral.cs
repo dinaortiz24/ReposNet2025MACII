e4bebecbc1156dddfc1fd52b1c5b879ea7a38710
Dina <53024899+dinaortiz24@users.noreply.github.com>
2025-05-19 08:40:34 -0600

    Add files via upload

---
+++ ManejoGeneralFiles/ClasesGeneralesFiles/ClGeneral.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+
+namespace ClasesGeneralesFiles
+{
+
+    public class ClGeneral
+    {
+        private  FileStream? fs;
+        private string? value;
+        //constructor
+
+        public ClGeneral() { }
+        public ClGeneral(FileStream fs, string value)
+        {
+            this.fs = fs ?? throw new ArgumentNullException(nameof(fs));
+            this.value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public void AddText(FileStream fs, string value)
+        {
+            this.fs = fs ?? throw new ArgumentNullException(nameof(fs));
+            this.value = value ?? throw new ArgumentNullException(nameof(value));
+
+            byte[] info = new UTF8Encoding(true).GetBytes(this.value);
+            this.fs.Write(info, 0, info.Length);
+        }
+    }
+}
---
+++ ManejoGeneralFiles/ManejoFiles1/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+internal class Test
+{
+    private static void Main(string[] args)
+    {
+        // hacer objero de clase
+        ClasesGeneralesFiles.ClGeneral objc = new ClasesGeneralesFiles.ClGeneral();
+            string path = @"C:\Users\dinao\Documents\MyTest.txt";
+        //borrar el archivo si existe
+        if (File.Exists(path)) { 
+          File.Delete(path);
+        }
+        //crear el archivo
+        using (FileStream fs = File.Create(path)) {
+              objc.AddText(fs, "Eba\n");
+            objc.AddText(fs, "Textto recopilao");
+
+            fs.Close();
+        }
+
+        //Abrir el archivo usar stran y leeer regresar los datos
+        using (FileStream fs = File.OpenRead(path))
+        {
+            byte[] b=new byte[1024];
+            UTF8Encoding tem = new UTF8Encoding(true);
+            while(fs.Read(b, 0,b.Length) > 0)
+            {
+                Console.WriteLine(tem.GetString(b));
+            }
+            fs.Close   ();
+        }
+    }
+   
+
+} //fin de clase
